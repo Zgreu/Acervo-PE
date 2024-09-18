bce378bddce61ab3cac08a253d02129fede1767f
Zgreu <alanlgd26@gmail.com>
2024-09-18 01:32:35 -0300

    Novo Começo

---
+++ Inicio/inicio.js
@@ -0,0 +1,14 @@
+    let sen = document.getElementById('senha')
+    let ola = document.getElementById('ola')
+    let nome = document.getElementById('nome')
+
+    function senha(){
+        if(sen.value == 'im'){
+            nome.style.opacity = "0"
+            nome.style.transition = 'display .3s ease'
+            nome.style.display = "none"
+            ola.innerHTML = '<a href="../Abas/poderes/poderes.html">OLA</a>'
+            
+            
+        }
+    }
