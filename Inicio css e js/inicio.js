c87c8a683ddd15cd363895dd3d49ba57fd335a95
Zgreu <alanlgd26@gmail.com>
2024-09-18 13:39:57 -0300

    senha concertada

--- Inicio css e js/inicio.js
+++ Inicio css e js/inicio.js
@@ -1,14 +1,25 @@
-    let sen = document.getElementById('senha')
-    let ola = document.getElementById('ola')
-    let nome = document.getElementById('nome')
+        var ola = document.getElementById('ola')
+            ola.style.display = "none"
+        var nome = document.getElementById('nome')
+        function senha(event){
 
-    function senha(){
+        let sen = document.getElementById('senha')
+
         if(sen.value == 'im'){
-            nome.style.opacity = "0"
-            nome.style.transition = 'display .3s ease'
-            nome.style.display = "none"
-            ola.innerHTML = '<a href="Abas/poderes/poderes.html">OLA</a>'
-            
+            nome.classList.toggle('mudando');
+            ola.style.display = 'block'
             
         }
-    }
+
+
+        nome.addEventListener("transitionend", () => {
+            nome.style.display = "none"
+            ola.classList.remove('mudando')
+            ola.classList.add('opacidade');
+
+        })
+
+        }
+
+
+        
