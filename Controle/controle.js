01a5b4118eadc102c9d8cab6c257157e4b2afae6
Zgreu <alanlgd26@gmail.com>
2024-09-18 16:29:34 -0300

    header

---
+++ Controle/controle.js
@@ -0,0 +1,11 @@
+var iconebt = document.getElementById('icone')
+var abas = document.getElementById('abas')
+
+function icone(){
+    iconebt.style.display = "none"
+
+    if(iconebt.style.display == "none")
+        abas.style.display = "block"
+
+    
+}
