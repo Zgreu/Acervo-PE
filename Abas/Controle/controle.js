873a6c40aaeebe54c8b992a8fdeefa381dd43937
Zgreu <alanlgd26@gmail.com>
2024-09-18 17:21:20 -0300

    barra de pesquisa

--- Abas/Controle/controle.js
+++ Abas/Controle/controle.js
@@ -1,8 +1,34 @@
 var iconebt = document.getElementById('icone')
 var abas = document.getElementById('abas')
+var pes = document.getElementById('pesquisa')
 
-botoes = ['poderes']
-iframep = ['ifpoderes', 'ifelementos']
+botoes = [document.getElementById('poderes')   
+]
+iframep = [document.getElementById('ifpoderes'), 
+        document.getElementById('ifelementos')]
+
+function pesquisa(){
+    switch(pes.value){
+        case 'poderes':
+        iframep[0].style.display = 'block'
+        iframep[1].style.display = 'none'
+        break
+
+        case 'elementos':
+        iframep[1].style.display = 'block'
+        iframep[0].style.display = 'none'
+        break
+
+
+        case '':
+        break
+        
+        default:
+        alert('coloque um valor valido e em letras minusculas')
+        break
+
+   }
+}
 
 function icone(){
     iconebt.style.display = "none"
@@ -16,7 +42,12 @@ function icone(){
 function poderes(){
 
     iframep[0].style.display = 'block'
-    alert('foi')
     if(iframep[0].style.display == 'block')
         iframep[1].style.display = 'none'
 }   
+
+function elementos(){
+    iframep[1].style.display = 'block'
+    if(iframep[1].style.display == 'block')
+        iframep[0].style.display = 'none'
+}
