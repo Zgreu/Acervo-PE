0b21063dfb552f139fc695ade104ca86722a783d
Zgreu <alanlgd26@gmail.com>
2024-09-20 00:39:07 -0300

    px to em e add pesquisas

---
+++ Controle/controle.js
@@ -0,0 +1,14 @@
+var iconebt = document.getElementById('icone')
+var abas = document.getElementById('abas')
+
+botoes = [document.getElementById('poderes')   
+]
+iframep = document.getElementById('ifpoderes') 
+       
+function poderes(){
+    iframep.src = "../Abas/Poderes/Poderes/poderes.html"
+}   
+
+function elementos(){
+    iframep.src = "../Abas/Poderes/Elementos/elementos.html"
+}
---
+++ Controle/pesquisa.js
@@ -0,0 +1,58 @@
+function pesquisa(){
+    var pes = document.getElementById('pesquisa')
+    pes = pes.value.toLowerCase()
+
+    switch(pes){
+        case 'poderes':
+            iframep.src = "../Abas/Poderes/Poderes/poderes.html"
+        break
+
+        case 'elementos':
+            iframep.src = "../Abas/Poderes/Elementos/elementos.html"
+        break
+
+        case 'tipos de habilidades':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'tipos':
+        iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+        
+        case 'tdh':
+        iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+        
+        case 'body':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'corpo':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'corpo a corpo':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'cpc':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'projeteis':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case 'foco':
+            iframep.src = "../Abas/Poderes/Tiposdehabilidades/tiposdehabilidade.html"
+        break
+
+        case '':
+        break
+
+        default:
+        alert('Esta pesquisa não vai a lugar nenhum')
+        break
+
+   }
+}
